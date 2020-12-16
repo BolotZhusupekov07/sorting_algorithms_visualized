b894ed821cde8eaefe3160e89e31c2763de34100
Bolot Zhusupekov <73470679+BolotZhusupekov07@users.noreply.github.com>
2020-12-16 11:28:43 +0600

    Update test.py

--- test.py
+++ test.py
@@ -40,7 +40,7 @@ def merge(arr, left_index, right_index, middle):
     left_copy_index = 0
     right_copy_index = 0
     sorted_index = left_index
-    while left_copy_index < len(left_copy) and right_copy_index < len(right_index):
+    while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
         if left_copy[left_copy_index] <= right_copy[right_copy_index]:
             arr[sorted_index] = left_copy[left_copy_index]
             left_copy_index += 1
@@ -62,7 +62,7 @@ def merge_sort(arr, left_index, right_index):
     if left_index >= right_index:
         return
     middle = (right_index + left_index) // 2
-    merge_sort(arr, left_index, middle + 1)
+    merge_sort(arr, left_index, middle)
     merge_sort(arr, middle + 1, right_index)
     merge(arr, left_index, right_index, middle)
 
